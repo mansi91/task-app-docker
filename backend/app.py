fc13f217dfa60dd43872458476aa1df31799a17d
mansi91 <mona.uikey@gmail.com>
2026-02-11 15:16:18 +0530

    Dockerized full-stack app: Nginx + Flask + PostgreSQL

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+﻿from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+tasks = []
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route('/tasks/<int:id>', methods=['POST'])
+def add_task(id):
+    tasks.append({'id': id, 'title': f'Task {id}'})
+    return jsonify({'message': 'Added'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
